66ae8c9d4fd3765924966a75129d4c01cb88d3a6
OLuciani <lucianioscar1@gmail.com>
2023-11-06 22:35:55 -0300

    Agrego 2 proyectos en Projects.js, cambio el CV y hago varias correcciones en general

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./projects.css";
 //import imagenclavedesol from "../img/clave-de-sol.png";
 import imagenlucianiautomoviles from "../img/captura_pantalla-home.png";
-import imagenportfolio from "../img/portfolio.png";
+//import imagenportfolio from "../img/portfolio.png";
+import imagenhernandezcabañas from "../img/hernandez_cabañas.png";
+import imagennearbytour from "../img/Imagen-NearByTour1.jpeg";
+import imagenMeddyplus from "../img/Imagen-Meddyplus.jpeg";
 
 function Projects() {
   return (
@@ -10,7 +13,6 @@ function Projects() {
       <h2 className="titulo-projects">Projects</h2>
 
       <div className="caja-madre">
-
         {/* <div className="caja-projects">
           <div className="div-text-project">
             <p className="text-projects">CLAVE DE SOL:<br/>Proyecto grupal de Ecommerce creado con Node JS, Express y MySQL en el marco de Digital House.</p>
@@ -21,31 +23,142 @@ function Projects() {
 
         <div className="caja-projects">
           <div className="caja-luciani-automoviles">
+            <div className="div-text-project">
               <div className="div-text-project">
-                <p className="text-projects">LUCIANI AUTOMOVILES:<br/>Mern Stack Project usando Netlify para el Frontend (creado en React Js), Render para el Backend (creado en Node Js y Express Js) y MongoDB Atlas para la Base de Datos.</p> 
+                <span className="text-projects">
+                  <span className="title-text-projects">NearByTour:</span>
+                  <br /> 
+                  NearByTour, una aplicación versátil que te permite descubrir tesoros ocultos y experiencias auténticas en destinos locales de todo el mundo, al mismo tiempo que te brinda la posibilidad de contribuir tus propios descubrimientos a la comunidad de viajeros.
+                  <br />
+                  Herramientas utilizadas: En el frontend se utilizó Next js 13,
+                  Typescript y Tailwind css. En el backend se utilizó Next js,
+                  Javascript, Firebase, Cloudinary, Postaman y Algolia. Los Diseñadores UX UI emplearon Figma.
+                </span>
               </div>
 
-            <a href="https://luciani-automoviles.netlify.app/">
+              <a href="https://www.youtube.com/watch?v=Hc3N-QXwT7Q">
+                <div className="contenedor-presentación">
+                  <p>Video presentación del proyecto NearByTour</p>
+                </div>
+              </a>
+            </div>
+
+            <a href="https://nearbytour.vercel.app/home">
               <div className="div-image-project">
-                <img className="image-project" src={imagenlucianiautomoviles} alt="imagen-luciani-automoviles" />
+                <img
+                  className="image-project"
+                  src={imagennearbytour}
+                  alt="Imágen NearByTour"
+                />
               </div>
               <div className="div-button-ver-proyecto">
                 <p className="button-ver-proyecto">Ver Proyecto</p>
               </div>
             </a>
           </div>
-        </div>  
+        </div>
 
         <div className="caja-projects">
+          <div className="caja-luciani-automoviles">
+            <div className="div-text-project">
+              <div className="div-text-project">
+                <span className="text-projects">
+                  <span className="title-text-projects">Meddyplus:</span>
+                  <br />
+                  Herramientas utilizadas: En el frontend se utilizó Vite, React js,
+                  Typescript y Tailwind css. En el backend se utilizó Node js,
+                  Typecript y SQL.
+                </span>
+              </div>
+
+              <a href="https://d3ml0dc5hiowr8.cloudfront.net/meddyplus.mp4">
+                <div className="contenedor-presentación">
+                  <p>Video presentación del proyecto Meddyplus</p>
+                </div>
+              </a>
+            </div>
+
+            <a href="https://meddyplus.netlify.app/">
+              <div className="div-image-project">
+                <img
+                  className="image-project"
+                  src={imagenMeddyplus}
+                  alt="Imágen Meddyplus"
+                />
+              </div>
+              <div className="div-button-ver-proyecto">
+                <p className="button-ver-proyecto">Ver Proyecto</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div className="caja-projects">
+          <div className="caja-luciani-automoviles">
+            <div className="div-text-project">
+              <span className="text-projects">
+                <span className="title-text-projects">Hernandez Cabañas:</span>
+                <br />
+                Mern Stack Project. Se utiliza Netlify para el deploy del Frontend y Render para el Backend. Base de datos en MongoDB Atlas. App para reservas y alquiler de cabañas.
+              </span>
+            </div>
+
+            <a href="https://hernandez-cabanas.netlify.app/">
+              <div className="div-image-project">
+                <img
+                  className="image-project"
+                  src={imagenhernandezcabañas}
+                  alt="imagen-hernandez-cabañas"
+                />
+              </div>
+              <div className="div-button-ver-proyecto">
+                <p className="button-ver-proyecto">Ver Proyecto</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        <div className="caja-projects">
+          <div className="caja-luciani-automoviles">
+            <div className="div-text-project">
+              <p className="text-projects">
+                LUCIANI AUTOMOVILES:
+                <br />
+                Mern Stack Project. Se utiliza Netlify para el deploy del Frontend y Render para el Backend. Base de datos en MongoDB Atlas. App de consecionaria de automóviles.
+              </p>
+            </div>
+
+            <a href="https://luciani-automoviles.netlify.app/">
+              <div className="div-image-project">
+                <img
+                  className="image-project"
+                  src={imagenlucianiautomoviles}
+                  alt="imagen-luciani-automoviles"
+                />
+              </div>
+              <div className="div-button-ver-proyecto">
+                <p className="button-ver-proyecto">Ver Proyecto</p>
+              </div>
+            </a>
+          </div>
+        </div>
+
+        {/* <div className="caja-projects">
           <div className="div-text-project">
-            <p className="text-projects">PORTFOLIO:<br/>Proyecto de este Portfolio creado con React Js.</p> 
+            <p className="text-projects">
+              PORTFOLIO:
+              <br />
+              Proyecto de este Portfolio creado con React Js.
+            </p>
           </div>
 
-          <img className="image-project" src={imagenportfolio} alt="imagen-portfolio" />
-        </div>  
-
+          <img
+            className="image-project"
+            src={imagenportfolio}
+            alt="imagen-portfolio"
+          />
+        </div> */}
       </div>
-
     </div>
   );
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -13,26 +13,27 @@ function Skills() {
 
       <div className="caja-skills">
         <ul className="box-skills1">
-          <li>Node Js<img className="imagen-node" src={imagennode} alt="imagen-nodeJS"/></li>   
-          <li>Javascript<i className="fa-brands fa-js icono-javascript" /></li>
-          <li>HTML<img className="imagen-html5" src={imagenhtml} alt="imagen-html5"/></li>    
-          <li>CSS3<i className="fa-brands fa-css3 icono-css3" /></li>   
+          <li>Next Js<i className="" /></li>
           <li>React Js<i className="fa-brands fa-react icono-react" /></li>
+          <li>Javascript<i className="fa-brands fa-js icono-javascript" /></li>
+          <li>Typescript<i className="" /></li>
+          <li>HTML<img className="imagen-html5" src={imagenhtml} alt="imagen-html5"/></li>    
+          <li>CSS3<i className="fa-brands fa-css3 icono-css3" /></li>
+          <li>Tailwind css<i className="fa-brands fa-react icono-react" /></li>
+          <li>Node Js<img className="imagen-node" src={imagennode} alt="imagen-nodeJS"/></li>   
           <li>Express<span className="span-express">Express</span><i className="fa-brands fa-js icono-javascript" /></li>
           <li>MySQL<img className="imagen-mysql" src={imagenmysql} alt="imagen-mysql"/></li>
-          <li>Creación y consumo de API's</li>
-          <li>Git<img className="imagen-git" src={imagengit} alt="imagen-git"/></li>    
-          
         </ul>
 
         <img className="photo-skills" src={imagenskills} alt="imagen-skills" />
 
         <ul className="box-skills2">
+          <li>Creación y consumo de API's</li>
+          <li>Git<img className="imagen-git" src={imagengit} alt="imagen-git"/></li> 
           <li>Metodologías Agiles - Scrum</li>
           <li>OPP</li>
           <li>Patrón de Arquitectura MVC</li>
           <li>Postman</li>
-          <li>Sequelize</li>
           <li>Vs Code</li>
           <li>Java<i className="fa-brands fa-java icono-java" /></li>    
           <li>Inglés - Sigo estudiando</li>
